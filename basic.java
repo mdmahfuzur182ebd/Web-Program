5c4595c02fec2b7aa57c19bb075d1a5cf0264e33
Mahfuzur <mdmahfuzur980@gmail.com>
2020-10-21 08:22:07 +0600

    some text added and basic java file created

---
+++ basic.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        System.out.println("My name is sajib");
+        System.out.println("My name is sajib");
+        int myfirst =123;
+        System.out.println(myfirst);
+        int a=90;
+        int b=123;
+        int c=89;
+        int sum=a+b+c;
+        System.out.println(sum);
+
+    }
+}
