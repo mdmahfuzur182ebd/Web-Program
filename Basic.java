b34226486a5c0fbe817472c5c01c6dd5c61869d2
Mahfuzur <mdmahfuzur980@gmail.com>
2020-10-25 08:22:46 +0600

    Basic java file created and some code added

---
+++ Basic.java
@@ -0,0 +1,20 @@
+Class ABC{
+
+   int a=10;
+   static int b-20;
+
+   Public static void main(String args[]){
+       ABC ob1=new ABC();
+       System.out.println(ob1.a);
+       System.ot.println(ob1.b);
+       ob1.a=1000;
+       ob1.b=2000;
+       System.out.println(ob1.a);
+       System.out.println(ob1.b);
+
+       ABC ob2=new ABC();
+       System.out.println(ob2.a);
+       System.out.println(ob2.b);
+   }
+
+}
